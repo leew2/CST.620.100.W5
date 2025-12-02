caa8344e67719139e6c9666305bde172abf1db5c
Wang Lee <leew2@csp.edu>
2025-12-02 09:26:59 -0800

    img cap test

--- src/img_caption.py
+++ src/img_caption.py
@@ -4,7 +4,7 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 def gen_img_cap(img_path):
-    process = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    process = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", use_fast=True)
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from img_caption import gen_img_cap
 
 def main():
-
+    path = "img/cat.jpg"
+    cap = gen_img_cap(path)
+    print("BLIP Caption:", cap)
 
 
 
