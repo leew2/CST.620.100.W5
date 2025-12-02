ebf3bc1e187d76125573e5387da727f3010992c0
Wang Lee <leew2@csp.edu>
2025-12-02 08:51:06 -0800

    img cap complete

--- src/img_caption.py
+++ src/img_caption.py
@@ -3,7 +3,18 @@ import torchvision.transforms as transforms
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
+def gen_img_cap(img_path):
+    process = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
+    img = Image.open(img_path).convert("RGB")
+    inputs = process(images=img, return_tensors="pt").to(device)
 
+    out = model.generate(**inputs)
+    caption = process.decode(out[0], skip_special_tokens=True)
+
+    return caption
 
