5614b8ac7b5d1ec58bfc4dabb4748b0256be2633
Wang Lee <leew2@csp.edu>
2025-12-02 09:57:16 -0800

    attention complete

--- src/attention_caption.py
+++ src/attention_caption.py
@@ -1,10 +1,13 @@
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from PIL import Image
+
 
 def get_attention_caption(img):
     model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-    processors = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    processors = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning", use_fast=True)
     tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
+    img = Image.open(img).convert("RGB")
     inputs = processors(images=img, return_tensors="pt")
     caption_id = model.generate(**inputs)
 
