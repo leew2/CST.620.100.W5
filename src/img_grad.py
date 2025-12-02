a0367cd47bddca3a2f498be12f645703665e8bd5
Wang Lee <leew2@csp.edu>
2025-12-02 09:03:03 -0800

    img grad

--- src/img_grad.py
+++ src/img_grad.py
@@ -1,5 +1,16 @@
 import torch.nn as nn
 import torchvision.models as models
+import plotlib.pyplot as plt
 
+def get_img_grad(img):
+    model = models.resnet18(pretrained=True)
+    model.eval()
 
+    img.requires_grad_(True)
+    output = model(img.unsqueeze(0))
+    class_idx = torch.argmax(output, dim=1)
+    output[0, class_idx].backward()
+
+    saliency = img.grad.data.abs().squeeze().cpu()
+    return saliency
 
