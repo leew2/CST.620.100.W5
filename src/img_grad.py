648c63d1744537c72c412a35733370fd241edbaf
Wang Lee <leew2@csp.edu>
2025-12-02 10:35:47 -0800

    img gradient done

--- src/img_grad.py
+++ src/img_grad.py
@@ -2,17 +2,28 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision.transforms as transforms
 
 def get_img_grad(img):
     model = models.resnet18(pretrained=True)
     model.eval()
 
-    img.requires_grad_(True)
-    output = model(img.unsqueeze(0))
-    class_idx = torch.argmax(output, dim=1)
-    output[0, class_idx].backward()
+    img = Image.open(img).convert("RGB")
+    preprocess = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+    img = preprocess(img)
+    img.requires_grad = True
 
-    saliency = img.grad.data.abs().squeeze().permute(1,2,0).cpu()
-    plt.imshow(saliency.numpy(), cmap='hot')
+    output = model(img.unsqueeze(0))
+    class_idx = torch.argmax(output)
+    output[0, class_idx].backward()
+    saliency = img.grad.abs().squeeze().permute(1, 2, 0).numpy()
+
+    plt.imshow(saliency, cmap='hot')
     plt.axis('off')
     plt.show()
+
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from img_caption import gen_img_cap
 from attention_caption import get_attention_caption
+from img_grad import get_img_grad
 
 def main():
     path = "img/cat.jpg"
-    cap = get_attention_caption(path)
-    print("Attention Caption:", cap)
+    get_img_grad(path)
 
 
 
